86d5e0880fd009298f57ba38880bc7feded1e286
Tim Primmer <timprimmer@gmail.com>
2021-07-14 17:38:23 -0500

    added more employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,3 +8,30 @@ test("creates an employee object", () => {
   expect(employee.email).toBe("test@email.com");
 
 });
+
+test("returns the name via getName()", () => {
+  const employee = new Employee("John", 1, "test@email.com");
+
+  expect(employee.getName()).toBe("John");
+
+});
+
+test("returns the id via getId()", () => {
+  const employee = new Employee("Tim", 5, "test@email.com");
+
+  expect(employee.getId()).toBe(5);
+
+});
+
+test("returns the email via getEmail()", () => {
+  const employee = new Employee("Tim", 1, "newtest@gmail.com");
+
+  expect(employee.getEmail()).toBe("newtest@gmail.com");
+
+});
+test("returns 'employee' via getRole()", () => {
+  const employee = new Employee("Tim", 1, "test@email.com");
+
+  expect(employee.getRole()).toBe("Employee");
+
+});
