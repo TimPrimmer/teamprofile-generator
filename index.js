2318c123e2c7cd7444b6d09ce5f67048b0b364f9
Tim Primmer <timprimmer@gmail.com>
2021-07-14 17:19:26 -0500

    created mock html and css styling

---
+++ index.js
@@ -0,0 +1,62 @@
+// packages needed for this application
+const inquirer = require('inquirer');
+//const fs = require('fs');
+const myArgs = process.argv.slice(2);
+
+// used if the user passes in the "test" parameter when starting the node app
+let mockData = {
+
+};
+
+
+// our giant questions array for inquirer
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the team managers name?",
+    validate(value) {
+      if (!value) {
+        console.log("please enter in a name!")
+        return false;
+      }
+      else { return true; }
+    }
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the team managers ID?",
+    validate(value) {
+      if (!value) {
+        console.log("please enter in an ID!")
+        return false;
+      }
+      else { return true; }
+    }
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the team managers email?",
+    validate(value) {
+      if (!value) {
+        console.log("please provide a email!")
+        return false;
+      }
+      else { return true; }
+    }
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the office number?",
+    validate(value) {
+      if (!value) {
+        console.log("please enter in an office number!")
+        return false;
+      }
+      else { return true; }
+    }
+  }
+];
